d5d8099c84b93f5179477a983bab7b24d79caa66
Freem11 <freem1985@gmail.com>
2021-09-21 12:28:24 -0700

    spinner2

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+const sentence = "|/-\|/-\|";
+
+let delay = 100;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write('\r' + char);
+  }, delay);
+  delay += 200;
+}
+
+setTimeout(() => {
+  process.stdout.write('\ ');
+}, delay+1);
+
